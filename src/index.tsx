809f0e60ed59a5facbffeb8817d908a2603f654d
olamileke <olamileke.f@gmail.com>
2022-07-17 21:36:28 +0100

    chore: updated app to runtime env variables in prod

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,32 @@
-import { ColorModeScript } from "@chakra-ui/react"
-import * as React from "react"
-import ReactDOM from "react-dom"
-import { App } from "./App"
-import reportWebVitals from "./reportWebVitals"
-import * as serviceWorker from "./serviceWorker"
+import { ColorModeScript } from "@chakra-ui/react";
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from "./serviceWorker";
+
+declare global {
+  interface Window {
+    __env__: { [key: string]: string };
+  }
+}
+
+window.__env__ = window.__env__ || {};
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript />
     <App />
   </React.StrictMode>,
-  document.getElementById("root"),
-)
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
